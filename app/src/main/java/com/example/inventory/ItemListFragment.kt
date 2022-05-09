fe14551b0874dafed7d30238856a3de2072861bb
Muntaka Helali <helali.muntaka@gmail.com>
2022-05-09 03:45:45 -0400

    Added a click handler to the RecyclerView to navigate the app to the Item Detail Screen.

--- app/src/main/java/com/example/inventory/ItemListFragment.kt
+++ app/src/main/java/com/example/inventory/ItemListFragment.kt
@@ -52,7 +52,8 @@ class ItemListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val adapter = ItemListAdapter {
-
+            val action = ItemListFragmentDirections.actionItemListFragmentToItemDetailFragment(it.id)
+            this.findNavController().navigate(action)
         }
         binding.recyclerView.adapter = adapter
         viewModel.allItems.observe(this.viewLifecycleOwner) {
