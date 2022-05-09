924a370aa70ade2857fa88091e43878fc80ef625
Muntaka Helali <helali.muntaka@gmail.com>
2022-05-09 04:00:47 -0400

    Binded the data that the user selects from the Item List Screen, to the Item Detail Screen.

--- app/src/main/java/com/example/inventory/ItemDetailFragment.kt
+++ app/src/main/java/com/example/inventory/ItemDetailFragment.kt
@@ -22,8 +22,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.inventory.data.Item
 import com.example.inventory.databinding.FragmentItemDetailBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
@@ -31,8 +33,15 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
  * [ItemDetailFragment] displays the details of the selected item.
  */
 class ItemDetailFragment : Fragment() {
-    private val navigationArgs: ItemDetailFragmentArgs by navArgs()
 
+    lateinit var item: Item
+    private val viewModel: InventoryViewModel by activityViewModels{
+        InventoryViewModelFactory(
+            (activity?.application as InventoryApplication).database.itemDao()
+        )
+    }
+
+    private val navigationArgs: ItemDetailFragmentArgs by navArgs()
     private var _binding: FragmentItemDetailBinding? = null
     private val binding get() = _binding!!
 
@@ -73,5 +82,23 @@ class ItemDetailFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val id = navigationArgs.itemId
+        viewModel.retrieveItem(id).observe(this.viewLifecycleOwner){
+            selectedItem ->
+            item = selectedItem
+            bind(item)
+        }
+    }
+
+    private fun bind(item: Item){
+        binding.apply {
+            itemName.text = item.itemName
+            itemPrice.text = item.itemPrice.toString()
+            itemCount.text = item.quantityInStock.toString()
+        }
     }
 }
